60151a9f0ec6dffb1ee54d66afbeb8b10d62b528
Steven Pham <Phooom@gmail.com>
2018-05-22 19:48:08 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -8,6 +8,9 @@ from sqlalchemy.ext.automap import automap_base
 from sqlalchemy.orm import Session
 from sqlalchemy import create_engine
 
+from flask import Flask, jsonify, render_template
+
+app = Flask(__name__)
 
 #################################################
 # Database Setup
@@ -28,6 +31,35 @@ Demographics = Base.classes.demographics
 # Create our session (link) from Python to the DB
 session = Session(engine)
 
-# Testing session query functionality (record count in table matches df)
-row_count = session.query(Crashes.crash_id).count()
-print(row_count)
+@app.route("/")
+def index():
+    """Return the homepage."""
+    return render_template('index.html')
+
+
+@app.route('/years')
+def carYears():
+    """Return a list of sample names."""
+
+    # Use Pandas to perform the sql query
+    stmt = session.query(Crashes).statement
+    df = pd.read_sql_query(stmt, session.bind)
+    df.set_index('crash_year', inplace=True)
+
+    # Return a list of the column names (sample names)
+    return jsonify(list(df.columns))
+
+@app.route('/census')
+def demoYears():
+
+    # Use Pandas to perform the sql query
+    stmt = session.query(Demographics).statement
+    df = pd.read_sql_query(stmt, session.bind)
+    df.set_index('year', inplace=True)
+
+    # Return a list of the column names (sample names)
+    return jsonify(list(df.columns))
+
+
+if __name__ == "__main__":
+    app.run(debug=True)
